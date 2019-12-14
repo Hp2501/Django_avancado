0deca99f55efabab2bf0766117b5d6aa031828e2
Hp2501 <herbetyp@gmail.com>
2019-12-13 23:53:34 -0300

    criando os models e atualizando templates

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Cargo, Servico, Funcionario, Tecnologia
 
-# Register your models here.
+
+@admin.register(Cargo)
+class CargoAdmin(admin.ModelAdmin):
+    list_display = ('cargo', 'ativo', 'modificado')
+
+
+@admin.register(Servico)
+class ServicoAdmin(admin.ModelAdmin):
+    list_display = ('servico', 'icone', 'ativo', 'modificado')
+
+
+@admin.register(Funcionario)
+class FuncionarioAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'cargo', 'modificado', 'ativo')
+
+
+@admin.register(Tecnologia)
+class TecnologiasAdmin(admin.ModelAdmin):
+    list_display = ('tecnologia', 'icone', 'ativo', 'modificado')
